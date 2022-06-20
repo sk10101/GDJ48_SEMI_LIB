bfa98c0e058931a7020227dff2348521449d2968
ParkJonghyeon <sk10101@naver.com>
2022-06-20 16:30:09 +0900

    MainController, MemberController 기본 폼 추가 board_mapper 기본 폼 추가

---
+++ src/main/java/com/gdj/lib/controller/MemberController.java
@@ -0,0 +1,12 @@
+package com.gdj.lib.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+}
