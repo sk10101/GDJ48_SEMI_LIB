ab30f50bc596be211e032f6bc6a9101adca740a4
LeeDaeHawn <dleoghks333@naver.com>
2022-06-20 20:28:33 +0900

    BoardDTO 에 공지사항 관련 게터세터 추가

---
+++ src/main/java/com/gdj/lib/dto/BoardDTO.java
@@ -0,0 +1,44 @@
+package com.gdj.lib.dto;
+
+import java.sql.Date;
+
+public class BoardDTO {
+
+	private int notice_id;
+	private int mb_id;
+	private Date notice_date;
+	private String notice_title;
+	private String notice_content;
+	public int getNotice_id() {
+		return notice_id;
+	}
+	public void setNotice_id(int notice_id) {
+		this.notice_id = notice_id;
+	}
+	public int getMb_id() {
+		return mb_id;
+	}
+	public void setMb_id(int mb_id) {
+		this.mb_id = mb_id;
+	}
+	public Date getNotice_date() {
+		return notice_date;
+	}
+	public void setNotice_date(Date notice_date) {
+		this.notice_date = notice_date;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	
+	
+}
