f52957373a64477e17dbc7589aca1fc9e93b698b
LeeDaeHawn <dleoghks333@naver.com>
2022-06-20 20:49:12 +0900

    공지사항 페이지 이동에 로그 추가

--- src/main/java/com/gdj/lib/controller/BoardController.java
+++ src/main/java/com/gdj/lib/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.gdj.lib.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gdj.lib.service.BoardService;
 
 @Controller
 public class BoardController {
