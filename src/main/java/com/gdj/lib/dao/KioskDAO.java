39972a9b4f6c76df6bb066fb7e34a15d09f013ee
hyeon.seung-jin <hhjab0206@gmail.com>
2022-06-21 15:02:25 +0900

    로그인 후 메인페이지 보기

--- src/main/java/com/gdj/lib/dao/KioskDAO.java
+++ src/main/java/com/gdj/lib/dao/KioskDAO.java
@@ -2,4 +2,6 @@ package com.gdj.lib.dao;
 
 public interface KioskDAO {
 
+	String login(String id, String pw);
+
 }
--- src/main/java/com/gdj/lib/service/KioskService.java
+++ src/main/java/com/gdj/lib/service/KioskService.java
@@ -1,8 +1,22 @@
 package com.gdj.lib.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gdj.lib.dao.KioskDAO;
 
 @Service
 public class KioskService {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired KioskDAO dao;
+	
+	public String login(String id, String pw) {
+		logger.info("키오스크 로그인 서비스 도착");
+		return dao.login(id,pw);
+	}
 
 }
