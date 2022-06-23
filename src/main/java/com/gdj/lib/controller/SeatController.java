79a799b22bb046672d0d934488053607499323c8
hyeon.seung-jin <hhjab0206@gmail.com>
2022-06-23 10:41:42 +0900

    좌석 상태 컨트롤러에 찍기

--- src/main/java/com/gdj/lib/controller/SeatController.java
+++ src/main/java/com/gdj/lib/controller/SeatController.java
@@ -1,5 +1,7 @@
 package com.gdj.lib.controller;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.gdj.lib.dto.SeatDTO;
 import com.gdj.lib.service.SeatService;
 
 @Controller
@@ -21,6 +24,13 @@ public class SeatController {
 	@RequestMapping(value = "/seat.go")
 	public String webSeat(Model model) {
 		logger.info("열람실 페이지 이동");
+		ArrayList<SeatDTO> list = service.list();
+		logger.info("list 개수: "+list.size());
+		for (int i = 0; i < list.size(); i++) {
+			logger.info(i+1+"번 : "+list.get(i).getSeat_status());
+		}
+		// logger.info("1번 : "+list.get(0).getSeat_status());
+		model.addAttribute("list", list);
 		return "seat/seat";
 	}
 }
